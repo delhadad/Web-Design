418582684e9ec2dc47709ee637151a6e21454868
Duaa <112613757+delhadad@users.noreply.github.com>
2024-01-16 17:50:09 +0300

    Add files via upload

---
+++ Kindergarten Website/script.js
@@ -0,0 +1,22 @@
+let loginForm = document.querySelector('.login-form');
+
+
+//*Login Button Click
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+
+//Window Scroll
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
